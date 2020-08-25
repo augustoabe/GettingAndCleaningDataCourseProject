d2274728e649102391d10edf923e0c09bb254a49
augustoabe <augusto.takeo.ismart@gmail.com>
2020-08-25 17:19:13 -0300

    Code for make tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -9,52 +9,30 @@ index <- sort(c(indexMean, indexStd))
 joint <- joint[,index]
 features <- features[index,2]
 
-
-
 labelsTest <- read.table("UCI HAR Dataset/test/y_test.txt")
 labelsTrain <- read.table("UCI HAR Dataset/train/y_train.txt") 
-classLabels <- rbind(labelsTest, labelsTrain)
-classLabels[,1] <- as.character(classLabels[,1])
+joint$actions <- rbind(labelsTest, labelsTrain)
+
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+joint$subject <- rbind(subjectTest, subjectTrain)
+
+tidyData <- data.frame()
+len <- length(names(joint))-2
+for( i in 1:30 ){
+        for( j in 1:6 ){
+                df <- filter( joint, actions == j & subject == i )
+                df1 <- data.frame( subject = c(i), actions = c(j) )
+                for( k in 1:len ){
+                        df[,k] <- as.numeric(df[,k])
+                        df1[,features[k]] <- mean(df[,k])
+                }
+                tidyData <- rbind(tidyData, df1)
+        }
+}
 actlabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 actlabels[,1] <- as.character(actlabels[,1])
+tidyData[,2] <- as.character(tidyData[,2])
 for( i in 1:6 ){
-        classLabels[,1] <- sub( actlabels[i,1], actlabels[i,2], classLabels[,1] )
+        tidyData[,2] <- sub( actlabels[i,1], actlabels[i,2], tidyData[,2] )
 }
-joint$actions <- classLabels
-
-walk <- filter( joint, actions == "WALKING" )
-walkup <- filter( joint, actions == "WALKING_UPSTAIRS" )
-walkdown <- filter( joint, actions == "WALKING_DOWNSTAIRS" )
-sit <- filter( joint, actions == "SITTING" )
-stand <- filter( joint, actions == "STANDING" )
-lay <- filter( joint, actions == "LAYING" )
-
-mwalk <- c()
-mwalkup <- c()
-mwalkdown <- c()
-msit <- c()
-mstand <- c()
-mlay <- c()
-
-for( i in 1:ncol(walk) ){
-        mwalk <- c( mwalk, mean( walk[,i] ) )
-        mwalkup <- c( mwalkup, mean( walkup[,i] ) )
-        mwalkdown <- c( mwalkdown, mean( walkdown[,i] ) )
-        msit <- c( msit, mean( sit[,i] ) )
-        mstand <- c( mstand, mean( stand[,i] ) )
-        mlay <- c( mlay, mean( lay[,i] ) )
-}
-
-tidyData <- data.frame(row.names = c("WALKING", "WALKING_UPSTAIRS", 
-                                     "WALKING_DOWNSTAIRS", "SITTING",
-                                     "STANDING", "LAYING"))
-
-for( i in 1:length(features) ){
-        tidyData[,features[i]] <- c(mwalk[i], mwalkup[i], mwalkdown[i], 
-                                    msit[i], mstand[i], mlay[i])
-}
-
-
-
-
-
